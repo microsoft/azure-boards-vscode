9f32f9aeb188fe0819572c204c93a4375ec35f94
Alex Nichols <19493383+alexcnichols@users.noreply.github.com>
2019-04-14 14:32:02 -0700

    Add inline mention control to tree item
    #30

--- src/workitems/commands.ts
+++ src/workitems/commands.ts
@@ -18,51 +18,49 @@ export function registerCommands() {
     //vscode.window.showInformationMessage("Refresh work items list");
   });
 
-  vscode.commands.registerCommand(
-    "azure-boards.mention-work-item",
-    workItemId => {
-      const gitExtension = vscode.extensions.getExtension<GitExtension>(
-        "vscode.git"
-      );
-      if (gitExtension) {
-        const git = gitExtension.exports.getAPI(1);
-        if (git.repositories.length) {
-          // determine whether source control is github.com, if so prefix mention ID syntax with "AB". To-Do: determine if GitHub Enterprise (non "github.com" host)
-          let mentionSyntaxPrefix: string = ``;
-          const activeRemotes: Remote[] = [];
-          const originRemotes = git.repositories[0].state.remotes.find(
-            remote => remote.name === "origin"
-          );
-          if (originRemotes) {
-            activeRemotes.push(originRemotes);
-            const remoteUrl =
-              activeRemotes[0].fetchUrl || activeRemotes[0].pushUrl || "";
-            const remoteUri = vscode.Uri.parse(remoteUrl);
-            const authority = remoteUri.authority;
-            const matches = /^(?:.*:?@)?([^:]*)(?::.*)?$/.exec(authority);
-            if (
-              matches &&
-              matches.length >= 2 &&
-              matches[1].toLowerCase() === "github.com"
-            ) {
-              mentionSyntaxPrefix = `AB`;
-            }
+  vscode.commands.registerCommand("azure-boards.mention-work-item", ars => {
+    const workItemId = ars.workItemId | ars;
+    const gitExtension = vscode.extensions.getExtension<GitExtension>(
+      "vscode.git"
+    );
+    if (gitExtension) {
+      const git = gitExtension.exports.getAPI(1);
+      if (git.repositories.length) {
+        // determine whether source control is github.com, if so prefix mention ID syntax with "AB". To-Do: determine if GitHub Enterprise (non "github.com" host)
+        let mentionSyntaxPrefix: string = ``;
+        const activeRemotes: Remote[] = [];
+        const originRemotes = git.repositories[0].state.remotes.find(
+          remote => remote.name === "origin"
+        );
+        if (originRemotes) {
+          activeRemotes.push(originRemotes);
+          const remoteUrl =
+            activeRemotes[0].fetchUrl || activeRemotes[0].pushUrl || "";
+          const remoteUri = vscode.Uri.parse(remoteUrl);
+          const authority = remoteUri.authority;
+          const matches = /^(?:.*:?@)?([^:]*)(?::.*)?$/.exec(authority);
+          if (
+            matches &&
+            matches.length >= 2 &&
+            matches[1].toLowerCase() === "github.com"
+          ) {
+            mentionSyntaxPrefix = `AB`;
           }
-          // add work item mention to new line if existing commit message, otherwise start with Fix mention
-          const existingCommitMessage: string =
-            git.repositories[0].inputBox.value;
-          let mentionText: string = ``;
-          if (existingCommitMessage) {
-            mentionText =
-              `\n` + `Fixes ` + mentionSyntaxPrefix + `#${workItemId}`;
-          } else {
-            mentionText = `Fix ` + mentionSyntaxPrefix + `#${workItemId} `;
-          }
-          git.repositories[0].inputBox.value += mentionText;
-          // navigate to the Source Control view
-          vscode.commands.executeCommand("workbench.view.scm");
         }
+        // add work item mention to new line if existing commit message, otherwise start with Fix mention
+        const existingCommitMessage: string =
+          git.repositories[0].inputBox.value;
+        let mentionText: string = ``;
+        if (existingCommitMessage) {
+          mentionText =
+            `\n` + `Fixes ` + mentionSyntaxPrefix + `#${workItemId}`;
+        } else {
+          mentionText = `Fix ` + mentionSyntaxPrefix + `#${workItemId} `;
+        }
+        git.repositories[0].inputBox.value += mentionText;
+        // navigate to the Source Control view
+        vscode.commands.executeCommand("workbench.view.scm");
       }
     }
-  );
+  });
 }
