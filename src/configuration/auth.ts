7981f8ec84dad8fba3a75059bd833c13f46c72d5
Alex Nichols <19493383+alexcnichols@users.noreply.github.com>
2019-06-10 22:43:31 -0700

    Add copywrite headers to src (#75)

--- src/configuration/auth.ts
+++ src/configuration/auth.ts
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
 import { openUrl } from "../util/open";
 import * as util from "util";
 import * as vscode from "vscode";
--- src/externals/git.d.ts
+++ src/externals/git.d.ts
@@ -1,7 +1,5 @@
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the MIT License. See License.txt in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
 
 import { Uri, SourceControlInputBox, Event, CancellationToken } from "vscode";
 
